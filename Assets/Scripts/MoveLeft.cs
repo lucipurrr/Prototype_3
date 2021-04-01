3ae093388111a44209739e19b153ba30c9de6548
odia <diakonova.oe@gmail.com>
2021-04-01 20:07:32 +0300

    testgitlab

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        
+        var menu = new GenericMenu();
+
+        Debug.Log("hueta");
+
+        PinObject pinnedItems = null;
+        Items item = null;
+        if (!pinnedItems.Contains(item.id))
+            menu.AddItem(new GUIContent("Pin"), false, () => pinnedItems.Add(item.id));
+        else return;
+    }
+}
+
+internal class Items
+{
+    public object id;
+}
+
+internal class PinObject
+{
+    public bool Contains(object id)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Add(object itemID)
+    {
+        throw new System.NotImplementedException();
+    }
+}
